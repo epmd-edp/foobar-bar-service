19d6841de076c131a64a901fede57743cd6df187
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2018-07-19 18:25:47 +0300

    [EPMDEDP-870] Consume rabbitmq credentials from secret
    Change-Id: I8b68e01a4d572ac06a33cf79b83a80e51e91511d

--- src/main/java/com/epam/edp/bar/config/RabbitMqConfig.java
+++ src/main/java/com/epam/edp/bar/config/RabbitMqConfig.java
@@ -4,9 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,34 +17,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
     public static final String QUEUE = "foobar-queue";
 
-    @Value("${spring.rabbitmq.host}")
-    private String rabbitmqHost;
-
-    @Value("${spring.rabbitmq.port}")
-    private Integer port;
-
-    @Value("${spring.rabbitmq.username}")
-    private String username;
-
-    @Value("${spring.rabbitmq.password}")
-    private String password;
-
     @Value("${spring.rabbitmq.exchange}")
     private String exchange;
-
     @Value("${spring.rabbitmq.routingkey}")
     private String routingKey;
-
-    @Bean
-    ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost(rabbitmqHost);
-        connectionFactory.setPort(port);
-        connectionFactory.setUsername(username);
-        connectionFactory.setPassword(password);
-        return connectionFactory;
-
-    }
 
     @Bean
     DirectExchange exchange() {
